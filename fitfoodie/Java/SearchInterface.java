85f461239634107dea7bf5248d7add5b77ecc366
Chirag Kamat <ck@Chirags-MacBook-Pro.local>
2018-03-21 15:38:55 -0600

    Initial Java files corresponding to class diagram

---
+++ fitfoodie/Java/SearchInterface.java
@@ -0,0 +1,9 @@
+
+public interface SearchInterface {
+	public Restaurant[] searchMealByLocation(Location address);
+	public Restaurant[] searchMealByCuisine(Cuisine cuisine);
+	public Restaurant[] searchMealByRadius(int radius);
+	public Restaurant[] searchMealByDietType(DietType dietType);
+	public Restaurant[] searchMealByRestaurantName(String name);
+	public Restaurant[] searchMealByNutrientQuantity(Nutrient nutrient);
+}
