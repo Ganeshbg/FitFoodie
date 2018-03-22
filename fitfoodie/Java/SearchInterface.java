7b1c42a9cea17f9909dc60454d584b100d376f61
0x90-n <36203274+0x90-n@users.noreply.github.com>
2018-03-21 22:48:04 -0600

    Delete SearchInterface.java

--- fitfoodie/Java/SearchInterface.java
+++
@@ -1,9 +0,0 @@
-
-public interface Searchable {
-	public Restaurant[] searchMealByLocation(Location address);
-	public Restaurant[] searchMealByCuisine(Cuisine cuisine);
-	public Restaurant[] searchMealByRadius(int radius);
-	public Restaurant[] searchMealByDietType(DietType dietType);
-	public Restaurant[] searchMealByRestaurantName(String name);
-	public Restaurant[] searchMealByNutrientQuantity(Nutrient nutrient);
-}
