66d5d4a3aa5bee7474e543c2f9177231265b0d73
Chirag Kamat <ck@Chirags-MacBook-Pro.local>
2018-04-08 20:21:26 -0600

    Adding area class to implement flyweight

---
+++ fitfoodie/Java/class-project/src/main/java/io/fitfoodie/food/Area.java
@@ -0,0 +1,16 @@
+package io.fitfoodie.food;
+
+public class Area {
+	private String zipCode;
+	private String state;
+	private String country;
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+}
