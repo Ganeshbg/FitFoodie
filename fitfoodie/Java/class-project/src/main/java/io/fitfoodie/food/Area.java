20984ef4ea37c925e3f519a8d4923875320e3786
Chirag Kamat <chka1976@colorado.edu>
2018-04-08 20:51:02 -0600

    Making the class an entity and adding more members

--- fitfoodie/Java/class-project/src/main/java/io/fitfoodie/food/Area.java
+++ fitfoodie/Java/class-project/src/main/java/io/fitfoodie/food/Area.java
@@ -1,7 +1,13 @@
 package io.fitfoodie.food;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Area {
+	@Id
 	private String zipCode;
+	private String name;
 	private String state;
 	private String country;
 
@@ -11,6 +17,30 @@ public class Area {
 
 	public void setZipCode(String zipCode) {
 		this.zipCode = zipCode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
 	}
 	
 }
