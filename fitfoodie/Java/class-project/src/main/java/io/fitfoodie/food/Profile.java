5b0bc2d68a904c429e0d7b6d1233e86a314e259a
AA <cs.alaraj@gmail.com>
2018-04-09 17:37:01 -0600

    Adding Profile class

---
+++ fitfoodie/Java/class-project/src/main/java/io/fitfoodie/food/Profile.java
@@ -0,0 +1,30 @@
+package io.fitfoodie.food;
+
+public abstract class Profile {
+
+	
+	
+	private DietType diet;
+	private Nutrient nutrients [];
+	public DietType getDiet() {
+		return diet;
+	}
+	public void setDiet(DietType diet) {
+		this.diet = diet;
+	}
+	public Nutrient[] getNutrients() {
+		return nutrients;
+	}
+	public void setNutrients(Nutrient[] nutrients) {
+		this.nutrients = nutrients;
+	}
+	
+	
+	
+	
+	
+	public boolean deleteProfile(){
+		
+		return true;
+	}
+}
