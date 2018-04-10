391e033cae3307f5d0e8b290b9f931be8dba672d
Chirag Kamat <chka1976@colorado.edu>
2018-04-10 16:48:24 -0600

    Flyweight design pattern

--- fitfoodie/Java/class-project/src/main/java/io/fitfoodie/food/Area.java
+++ fitfoodie/Java/class-project/src/main/java/io/fitfoodie/food/Area.java
@@ -1,11 +1,17 @@
 package io.fitfoodie.food;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 //import javax.persistence.Entity;
 //import javax.persistence.Id;
 
 //@Entity
 public class Area {
 	//@Id
+	
+	@Autowired
+	private AreaService areaService;
+	
 	private String zipCode;
 	private String name;
 	private String state;
@@ -42,9 +48,14 @@ public class Area {
 	public void setCountry(String country) {
 		this.country = country;
 	}
+	
+	public Area() {
+		
+	}
 
 	public Area(String zipCode, String name, String state, String country) {
 		super();
+		
 		this.zipCode = zipCode;
 		this.name = name;
 		this.state = state;
