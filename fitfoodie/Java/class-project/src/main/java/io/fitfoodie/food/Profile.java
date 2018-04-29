730ee186cc81689282a083641b5d029ca9723cb7
0x90-n <36203274+0x90-n@users.noreply.github.com>
2018-04-29 00:13:28 -0600

    Create Profile.java

---
+++ fitfoodie/Java/class-project/src/main/java/io/fitfoodie/food/Profile.java
@@ -0,0 +1,45 @@
+package io.fitfoodie.food;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="@class")
+public class Profile {
+
+	
+	
+	private DietType diet;
+	private String id;
+	//private Nutrient nutrients [];
+	private List<Nutrients> nutrients = new ArrayList<>(Arrays.asList());
+	
+	
+	public DietType getDiet() {
+		return diet;
+	}
+	public void setDiet(DietType diet) {
+		this.diet = diet;
+	}
+	public List<Nutrients> getNutrients() {
+		return nutrients;
+	}
+	public void setNutrients(List<Nutrients> nutrients) {
+		this.nutrients = nutrients;
+	}
+		
+	public boolean deleteProfile(){
+		
+		return true;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
