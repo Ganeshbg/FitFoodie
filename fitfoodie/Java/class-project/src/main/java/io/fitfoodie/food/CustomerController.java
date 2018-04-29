936794d1e6b6f3f19ea740dcf68e79a5c57f1275
0x90-n <36203274+0x90-n@users.noreply.github.com>
2018-04-29 00:16:30 -0600

    Create CustomerController.java

---
+++ fitfoodie/Java/class-project/src/main/java/io/fitfoodie/food/CustomerController.java
@@ -0,0 +1,39 @@
+package io.fitfoodie.food;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+	@Autowired
+	private CustomerService customerService;
+	
+	@RequestMapping("/customer/get/{id}")
+	public Customer getCustomer(@PathVariable String id){
+		return customerService.getCustomer(id);
+		
+	}
+
+	@RequestMapping(method=RequestMethod.PUT, value="/customer/updateprofile/{id}/{profileId}")
+	public void updateProfile(@PathVariable String id, @PathVariable String profileId, Profile p ) {
+		
+		customerService.updateCustomerProfile(id,profileId,p);
+		
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/profile")
+	public void addNutrient(@RequestBody Nutrients nutrient)
+	{
+		//not implemented
+		//customerService.addNutrient(nutrient);
+	}
+
+	
+}
